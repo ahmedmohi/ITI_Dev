0bcf041850a68fcdbd2b7b536a552ad8821d7775
MOhi <eng_mohi@ymail.com>
2023-04-19 11:31:27 +0200

    Revert "change debug"
    This reverts commit 867dd08cb0b05ef2203df10f8d2e89f233527b1e.

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("H");
+        Debug.Log("Hello world");
     }
 
     // Update is called once per frame
