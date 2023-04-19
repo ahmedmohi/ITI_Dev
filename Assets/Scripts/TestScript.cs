d822e69818a8fce58e67267b34d609fbf6d6f591
MOhi <eng_mohi@ymail.com>
2023-04-19 11:23:11 +0200

    remove log

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,6 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world");
     }
 
     // Update is called once per frame
