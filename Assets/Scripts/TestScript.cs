df039e944b3d5908f3cd15504a332b5aa8c901c0
MOhi <eng_mohi@ymail.com>
2023-04-19 11:37:49 +0200

    AI change

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -7,7 +7,7 @@ public class TestScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello world");
+        Debug.Log("AI Hello world");
     }
 
     // Update is called once per frame
